5b94667e916f6c15eeb95a13477875fde48450f1
Aeson Johnson <aeson.m.johnson@gmail.com>
2019-12-06 08:58:26 -0800

    aeson-johnson

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,9 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header(items) {
+    const headerDiv = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+}
